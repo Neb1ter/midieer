8dfcfa57b035cfde69d6af0b29727e7ff8051d87
Neb1ter <neb1ter@users.noreply.github.com>
2026-03-04 21:12:02 +0800

    fix: remove local cacheDir config causing build issues on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  cacheDir: path.resolve(__dirname, "../news/.vite_cache"),
+  plugins: [react()],),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
